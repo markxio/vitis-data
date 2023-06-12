af58be7414b48c7da76d9705251fff145557ed36
Gabriel RodrÃiguez-Canal <gabriel.rodriguez-canal@hpe.com>
2023-06-12 11:06:26 +0100

    Logic for resource utilisation moved into a class

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import argparse
-import re
+from resource_utilisation import ResourceUtilisation
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -10,41 +10,5 @@ if __name__ == "__main__":
     root_dir = args.root_dir
     kernel_name = args.kernel_name
 
-    rep_filename = f"{root_dir}/_x/reports/{kernel_name}/hls_reports/{kernel_name}_csynth.rpt"
-    f = open(rep_filename) 
-
-    txt = f.read()
-
-    #print(txt)
-
-    match_obj = re.search("== Utilization Estimates.+\+ Detail", txt, flags=re.DOTALL)
-    res_table = match_obj.group(0)
-
-    #total_match_obj = re.search("|Total.+(\d+).+(\d+).+(\d+).+(\d+)", res_table, flags=re.DOTALL)
-    sep = "\s*\|\s*"
-    total_match_obj = re.search(f"Total{sep}(\d+){sep}(\d+){sep}(\d+){sep}(\d+){sep}(\d+)", res_table, flags=re.DOTALL)
-    totals = {'bram': int(total_match_obj.group(1)),
-              'dsp': int(total_match_obj.group(2)),
-              'ff': int(total_match_obj.group(3)),
-              'lut': int(total_match_obj.group(4)),
-              'uram': int(total_match_obj.group(5))
-              }
-
-    avail_match_obj = re.search(f"Available{sep}(\d+){sep}(\d+){sep}(\d+){sep}(\d+){sep}(\d+)", res_table, flags=re.DOTALL)
-    avail = {'bram': int(avail_match_obj.group(1)),
-              'dsp': int(avail_match_obj.group(2)),
-              'ff': int(avail_match_obj.group(3)),
-              'lut': int(avail_match_obj.group(4))#,
-              #'uram': int(avail_match_obj.group(5))
-              }
-
-
-    util = {'bram': totals['bram'] / avail['bram'] * 100,
-             'dsp': totals['dsp'] / avail['dsp'] * 100,
-             'ff': totals['ff'] / avail['ff'] * 100,
-             'lut': totals['lut'] / avail['lut'] * 100#,
-             #'uram': avail['uram'] / totals['uram']
-              }
-
-    print("LUT\tFF\tBRAM\tDSP")
-    print(f"{util['lut']:.2f}\t{util['dsp']:.2f}\t{util['ff']:.2f}\t{util['lut']:.2f}")
+    obj_res_util = ResourceUtilisation(root_dir, kernel_name)
+    print(obj_res_util.get_resource_utilisation())
---
+++ resource_utilisation.py
@@ -0,0 +1,44 @@
+import re
+
+class ResourceUtilisation:
+    def __init__(self, root_dir, kernel_name):
+        self.root_dir = root_dir
+        self.kernel_name = kernel_name
+
+        rep_filename = f"{root_dir}/_x/reports/{kernel_name}/hls_reports/{kernel_name}_csynth.rpt"
+        f = open(rep_filename) 
+
+        txt = f.read()
+
+        match_obj = re.search("== Utilization Estimates.+\+ Detail", txt, flags=re.DOTALL)
+        res_table = match_obj.group(0)
+
+        sep = "\s*\|\s*"
+        total_match_obj = re.search(f"Total{sep}(\d+){sep}(\d+){sep}(\d+){sep}(\d+){sep}(\d+)", res_table, flags=re.DOTALL)
+        self.totals = {'bram': int(total_match_obj.group(1)),
+                  'dsp': int(total_match_obj.group(2)),
+                  'ff': int(total_match_obj.group(3)),
+                  'lut': int(total_match_obj.group(4)),
+                  'uram': int(total_match_obj.group(5))
+                  }
+
+        avail_match_obj = re.search(f"Available{sep}(\d+){sep}(\d+){sep}(\d+){sep}(\d+){sep}(\d+)", res_table, flags=re.DOTALL)
+        self.avail = {'bram': int(avail_match_obj.group(1)),
+                  'dsp': int(avail_match_obj.group(2)),
+                  'ff': int(avail_match_obj.group(3)),
+                  'lut': int(avail_match_obj.group(4))#,
+                  #'uram': int(avail_match_obj.group(5))
+                  }
+
+
+        self.util = {'bram': self.totals['bram'] / self.avail['bram'] * 100,
+                 'dsp': self.totals['dsp'] / self.avail['dsp'] * 100,
+                 'ff': self.totals['ff'] / self.avail['ff'] * 100,
+                 'lut': self.totals['lut'] / self.avail['lut'] * 100#,
+                 #'uram': avail['uram'] / totals['uram']
+                  }
+
+
+
+    def get_resource_utilisation(self):
+        return self.util
